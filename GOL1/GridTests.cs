28fb40217bdaec92bb68eaee3539f1bacab359c5
Tom Liversidge <TomL@apadmi.com>
2013-04-08 20:44:36 +0100

    Adding unit tests

--- GOL1/GridTests.cs
+++ GOL1/GridTests.cs
@@ -19,21 +19,84 @@ namespace GOL1
         /// </summary>
 
         [Test]
-        public void TestUnderPopulation_Death()
+        public void CellDiesWithNoNeighbours()
         {
             // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-            var grid = new Grid(new Cell(1,1), new Cell(0,0));
+            var grid = new Grid(new Cell(1,1));
             grid.Evolve();
             Assert.IsFalse(grid.IsAlive(new Cell(1, 1)));
         }
 
         [Test]
-        public void TestUnderPopulation_Life()
+        public void CellDiesWithOneNeighbour()
         {
             // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+            var grid = new Grid(new Cell(0, 0), new Cell(0, 1));
+            grid.Evolve();
+            Assert.IsFalse(grid.IsAlive(new Cell(0, 0)));
+        }
+
+        [Test]
+        public void CellLivesWithTwoNeighbours()
+        {
+            // Any live cell with two or three live neighbours lives on to the next generation.
+            var grid = new Grid(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0));
+            grid.Evolve();
+            Assert.IsTrue(grid.IsAlive(new Cell(0, 0))); // cell had two neighbours, should survive
+        }
+
+        [Test]
+        public void CellLivesWithThreeNeighbours()
+        {
+            // Any live cell with two or three live neighbours lives on to the next generation.
             var grid = new Grid(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0), new Cell(1, 1));
             grid.Evolve();
+            Assert.IsTrue(grid.IsAlive(new Cell(0, 0))); // cell had three neighbours, should survive
+        }
+
+        [Test]
+        public void CellDiesWithFourNeighbours()
+        {
+            // Any live cell with more than three live neighbours dies, as if by overcrowding.
+            var grid = new Grid(new Cell(1, 1), new Cell(0, 1), new Cell(1, 0), new Cell(2, 1), new Cell(1,2));
+            grid.Evolve();
+            Assert.IsFalse(grid.IsAlive(new Cell(1, 1))); // cell had four neighbours, should die
+        }
+
+        [Test]
+        public void CellDiesWithFiveNeighbours()
+        {
+            // Any live cell with more than three live neighbours dies, as if by overcrowding.
+            var grid = new Grid(new Cell(1, 1), new Cell(0, 1), new Cell(1, 0), new Cell(2, 1), new Cell(1, 2), new Cell(0,0));
+            grid.Evolve();
+            Assert.IsFalse(grid.IsAlive(new Cell(1, 1))); // cell had four neighbours, should die
+        }
+
+        [Test]
+        public void CellDoesNotReviveWithTwoLiveNeighbours()
+        {
+            // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            var grid = new Grid(new Cell(0, 0), new Cell(0, 1));
+            grid.Evolve();
+            Assert.IsFalse(grid.IsAlive(new Cell(1, 1)));
+        }
+
+        [Test]
+        public void CellRevivesWithThreeLiveNeighbours()
+        {
+            // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            var grid = new Grid(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0));
+            grid.Evolve();
             Assert.IsTrue(grid.IsAlive(new Cell(1, 1)));
+        } 
+        
+        [Test]
+        public void CellDoesNotReviveWithFourLiveNeighbours()
+        {
+            // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            var grid = new Grid(new Cell(0, 0), new Cell(0, 1), new Cell(1, 0), new Cell(2,1));
+            grid.Evolve();
+            Assert.IsFalse(grid.IsAlive(new Cell(1, 1)));
         }
     }
 }
